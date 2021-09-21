1d6599dde5235899b6636fa8ef833115b14e78d1
Peter Svensson <peter.z.svensson@teliacompany.com>
2021-09-21 08:47:46 +0200

    module4 update

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,15 +12,13 @@ class Quiz extends Component {
 
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
-        let quizTag;
-        if (isQuizEnd) {
-            quizTag = <QuizEnd />
-        } else {
-            quizTag = <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-        }
         return (
             <div>
-                {quizTag}
+                {
+                    isQuizEnd
+                        ? (<QuizEnd />)
+                        : (<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />)
+                }
             </div>
         )
     }
